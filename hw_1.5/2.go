64f71bea26d106dc6e881789377005ecdaf4a1a3
Irina <irina@example.com>
2025-06-24 00:21:08 +0300

    hw_1.5_задача_2

---
+++ hw_1.5/2.go
@@ -0,0 +1,33 @@
+// Задача 2:
+// Напишите программу, которая подсчитывает количество гласных букв (а, е, ё, и, о, у, ы, э, ю, я) в введённой пользователем строке.
+
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func main() {
+
+	var str string
+
+	fmt.Println("Введите строку: ")
+	fmt.Scanln(&str)
+
+	glasnye := []rune{'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
+
+	count := 0
+	for _, element := range str {
+		change := unicode.ToLower(element)
+		for _, element1 := range glasnye {
+			if change == element1 {
+				count++
+			}
+
+		}
+
+	}
+	fmt.Printf("Количество гласных букв: %v", count)
+
+}
