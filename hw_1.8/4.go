64e6e47ed7a855415f621109d369f59c3e70ec1e
Irina <irina@example.com>
2025-07-07 00:50:31 +0300

    hw_1.8_задача_4

---
+++ hw_1.8/4.go
@@ -0,0 +1,24 @@
+// Задача 4
+// Создайте функцию sumAll, которая принимает произвольное количество целых чисел и возвращает их сумму.
+// Пример использования:
+//
+//	fmt.Println(sumAll(1, 2, 3)) // 6
+//	fmt.Println(sumAll(10, -2, 4, 7)) // 19
+package main
+
+import "fmt"
+
+func sumAll(nums ...int) int {
+	res := 0
+	for _, num := range nums {
+		res += num
+	}
+	return res
+}
+
+func main() {
+
+	fmt.Println(sumAll(1, 2, 3))
+	fmt.Println(sumAll(10, -2, 4, 7))
+
+}
